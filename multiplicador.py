c841c8e055c1ac4f1227e68d52549cadccbc8f09
KAUAN7CR7 <131807689+KAUAN7CR7@users.noreply.github.com>
2023-04-25 16:05:31 -0300

    Add files via upload

---
+++ multiplicador.py
@@ -0,0 +1,7 @@
+print("bem vindo ao multiplicador")
+num1=(input("digite um numero inteiro:"))
+num2=(input("digite outro numero inteiro:"))
+x1=int(num1)
+x2=int(num2)
+result=x1*x2
+print("o resultado da multiplicação e:",result)
---
+++ olamundo.py
@@ -0,0 +1,1 @@
+print("CR7 o Melhor do Mundo")
